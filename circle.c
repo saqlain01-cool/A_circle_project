1eff3f73a26c0bf68e6d4e88b2c3cd8cdc974f39
saqlain01-cool <mdsaqlainashraf@gmail.com>
2021-04-23 21:41:47 +0530

    add feature to compute the circumference of the circle

--- circle.c
+++ circle.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
-#define pi 3.14
+#define PI 3.14
 int main()
 {
     int radius;
-    float area;
+    float area,circumference;
     printf("Enter the radius of the circle:");
     scanf("%d",&radius);
-    area = pi *radius*radius;
+    area = PI *radius*radius;
     printf("Area of circle: %0.3f \n",area);
+    circumference= 2*PI*radius;
+    printf("circumference of circle: %0.3f \n",circumference);
     return 0;
 }
